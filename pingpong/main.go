f6134951a6c8c705dc8c3f9bb2dcc47f30786a05
Ariana Barbosa Vidal <91435768+ariana-vidal@users.noreply.github.com>
2025-10-30 13:49:55 -0300

    ping pong com goroutines

--- pingpong/main.go
+++ pingpong/main.go
@@ -1,1 +1,35 @@
 
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func ping(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func pong(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	c := make(chan string)
+
+	go ping(c)
+	go pong(c)
+	printer(c)
+}
